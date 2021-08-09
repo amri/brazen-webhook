9e2448657be4bcf43809839dc79d28aebc4edd95
Amri Hidayat <amri.hidayat@gmail.com>
2021-08-09 14:13:47 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+  
+  port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+  
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
