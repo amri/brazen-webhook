2c06fab803d4181db143c3f110527ef1669cf1b2
Amri Hidayat <amri.hidayat@gmail.com>
2021-08-09 14:14:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 	"net/http"
 )
 
-port := os.Getenv("PORT")
-	if port == "" {
-		port = "80"
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
