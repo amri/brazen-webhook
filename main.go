03e1dda1ef8bc283ff949e05b41a4b46fe7dc804
Amri Hidayat <amri.hidayat@gmail.com>
2021-08-11 17:22:05 +0800

    another library - test

--- main.go
+++ main.go
@@ -1,38 +1,125 @@
 package main
 
 import (
+	"crypto/x509"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
+
+	"io/ioutil"
+
+	"encoding/base64"
+	"encoding/xml"
+
+	saml2 "github.com/russellhaering/gosaml2"
+	"github.com/russellhaering/gosaml2/types"
+	dsig "github.com/russellhaering/goxmldsig"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	fmt.Println(err)
-	fmt.Println(string(body))
-	fmt.Fprintf(w, string(body), r.URL.Path[1:])
-}
-
 func main() {
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	res, err := http.Get("http://idp.oktadev.com/metadata")
+	if err != nil {
+		panic(err)
 	}
 
-	http.HandleFunc("/register/1", serveFiles)
-
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
-
-func serveFiles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
-	p := "." + r.URL.Path
-	if p == "./register/1" {
-		p = "./1.html"
+	rawMetadata, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
 	}
-	http.ServeFile(w, r, p)
+
+	metadata := &types.EntityDescriptor{}
+	err = xml.Unmarshal(rawMetadata, metadata)
+	if err != nil {
+		panic(err)
+	}
+
+	certStore := dsig.MemoryX509CertificateStore{
+		Roots: []*x509.Certificate{},
+	}
+
+	for _, kd := range metadata.IDPSSODescriptor.KeyDescriptors {
+		for idx, xcert := range kd.KeyInfo.X509Data.X509Certificates {
+			if xcert.Data == "" {
+				panic(fmt.Errorf("metadata certificate(%d) must not be empty", idx))
+			}
+			certData, err := base64.StdEncoding.DecodeString(xcert.Data)
+			if err != nil {
+				panic(err)
+			}
+
+			idpCert, err := x509.ParseCertificate(certData)
+			if err != nil {
+				panic(err)
+			}
+
+			certStore.Roots = append(certStore.Roots, idpCert)
+		}
+	}
+
+	// We sign the AuthnRequest with a random key because Okta doesn't seem
+	// to verify these.
+	randomKeyStore := dsig.RandomKeyStoreForTest()
+
+	sp := &saml2.SAMLServiceProvider{
+		IdentityProviderSSOURL:      metadata.IDPSSODescriptor.SingleSignOnServices[0].Location,
+		IdentityProviderIssuer:      metadata.EntityID,
+		ServiceProviderIssuer:       "http://example.com/saml/acs/example",
+		AssertionConsumerServiceURL: "http://localhost:8080/v1/_saml_callback",
+		SignAuthnRequests:           true,
+		AudienceURI:                 "http://example.com/saml/acs/example",
+		IDPCertificateStore:         &certStore,
+		SPKeyStore:                  randomKeyStore,
+	}
+
+	http.HandleFunc("/v1/_saml_callback", func(rw http.ResponseWriter, req *http.Request) {
+		err := req.ParseForm()
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		assertionInfo, err := sp.RetrieveAssertionInfo(req.FormValue("SAMLResponse"))
+		if err != nil {
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		if assertionInfo.WarningInfo.InvalidTime {
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		if assertionInfo.WarningInfo.NotInAudience {
+			rw.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		fmt.Fprintf(rw, "NameID: %s\n", assertionInfo.NameID)
+
+		fmt.Fprintf(rw, "Assertions:\n")
+
+		for key, val := range assertionInfo.Values {
+			fmt.Fprintf(rw, "  %s: %+v\n", key, val)
+		}
+
+		fmt.Fprintf(rw, "\n")
+
+		fmt.Fprintf(rw, "Warnings:\n")
+		fmt.Fprintf(rw, "%+v\n", assertionInfo.WarningInfo)
+	})
+
+	println("Visit this URL To Authenticate:")
+	authURL, err := sp.BuildAuthURL("")
+	if err != nil {
+		panic(err)
+	}
+
+	println(authURL)
+
+	println("Supply:")
+	fmt.Printf("  SP ACS URL      : %s\n", sp.AssertionConsumerServiceURL)
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
